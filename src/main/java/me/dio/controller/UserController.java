451ff1deabe04b2787d606752b27837a36b59862
Charles <charlesthiagosantos@gmail.com>
2024-07-29 21:16:43 -0300

    feat : projeto base Santander

---
+++ src/main/java/me/dio/controller/UserController.java
@@ -0,0 +1,36 @@
+package me.dio.controller;
+
+import me.dio.domain.model.User;
+import me.dio.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user){
+        User userCreate = userService.create(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(userCreate.getId())
+                .toUri();
+        return ResponseEntity.ok(userCreate);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+}
